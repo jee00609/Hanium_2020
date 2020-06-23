0c1ed191896b0d491d8181a09376ffc4578db887
Integral76 <31675804+jee00609@users.noreply.github.com>
2020-06-23 23:28:11 +0900

    얼굴 인식 사각형 중심점 출력
    라즈베리 파이 모터 구동을 위한 중심점 찾기 기능 추가

--- Hanium_opencv_face_detection/face_detection.py
+++ Hanium_opencv_face_detection/face_detection.py
@@ -9,25 +9,37 @@ face_cascade = cv2.CascadeClassifier('./data/haarcascade_frontalface_default.xml
 eye_cascade = cv2.CascadeClassifier('./data/haarcascade_eye.xml')
 
 
-
-#---얼굴 검출
-
-
-
 # 카메라 캡쳐 활성화
 # 웹 카메라는 0번!
 cap = cv2.VideoCapture(0)
 
+#얼굴 인식 될 때 사각형의 좌표값 출력하기 위해 정의
+centerw=0
+centerh=0
+
+#실시간으로 좌표 출력하기엔 너무 많은 좌표가 출력되기에 time 이 50 이 될때마다 출력 하기 위해 정의
+time=0
+
 while cap.isOpened():    
     ret, img = cap.read()  # 프레임 읽기
+    center = 0
     if ret:
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         # 얼굴 검출    
         faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(80,80))
         for(x,y,w,h) in faces:
             cv2.rectangle(img, (x,y), (x+w, y+h), (0, 255,0),2)
+            
+            centerw = (x+w)/2
+            centerh = (y+h)/2
+            
             roi = gray[y:y+h, x:x+w]
         cv2.imshow('face detect', img)
+        time=time+1
+        print
+        if(time==50):
+            print('(',centerw,centerh,')')
+            time=0
     else:
         break
     if cv2.waitKey(5) == 27: #키보드 esc 눌러주세용
